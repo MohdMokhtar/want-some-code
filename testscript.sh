29e26980c8bf5ac303fb02e837326be1ecd3b227
mohd_mokhtar <Sumaira@gmail.com>
2020-11-24 10:51:50 +0530

    jenkinstest

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-cd /root/sbm1/gateway-service/testscript.sh
+cd /home/rakesh/Documents/Mohd_Data/git_rep
 username=$1
 password=$2
 
@@ -7,10 +7,11 @@ if [[ "$username" = "mohd_mokhtar" && "password" = "Passw0rd" ]];
    then
    echo "My username is $username and password is $password"
 
-_GIT_RESULT=`git pull origin test | grep "Already up-to-date"`
+_GIT_RESULT=`git pull origin deshboard | grep "Already up-to-date"`
 if [ -z _GIT_RESULTS ]
    then
    echo "No change in current branch"
 else
    echo "There are some changes in the current branch"
-   echo "$_GIT_RESULTS"
+   echo "$GIT_RESULTS"
+fi
